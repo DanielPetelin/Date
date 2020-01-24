0c8cbb5e93daf885c6ed1ff0f9b4daee68d32282
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-24 20:31:02 +0300

    end project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.querySelector('button').onclick = function() {
+    var dateStart = document.querySelector('#start-date').value;    
+    var dateEnd = document.querySelector('#end-date').value;   
+    dateStart = Date.parse(dateStart); 
+    dateEnd = Date.parse(dateEnd);
+    var out = document.querySelector('#out');
+    for (var i = dateStart; i <= dateEnd; i = i + 24 * 60 * 60 * 1000) {
+        out.innerHTML += new Date(i).toISOString().substr(0, 10) + '<br>';
+    }; 
+};
