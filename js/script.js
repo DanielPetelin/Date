99437663ca71772de365e6e40db0ccdc4146aa43
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-24 20:43:05 +0300

    final commit

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ document.querySelector('button').onclick = function() {
     dateStart = Date.parse(dateStart); 
     dateEnd = Date.parse(dateEnd);
     var out = document.querySelector('#out');
-    for (var i = dateStart; i <= dateEnd; i = i + 24 * 60 * 60 * 1000) {
-        out.innerHTML += new Date(i).toISOString().substr(0, 10) + '<br>';
-    }; 
+    if (dateStart < dateEnd) {
+        for (var i = dateStart; i <= dateEnd; i = i + 24 * 60 * 60 * 1000) {
+            out.innerHTML += new Date(i).toISOString().substr(0, 10) + '<br>';
+        };
+    };
 };
